920e378b711c98faff7ae6b4ed06ac45785d0ee7
Justin Gruca <jgruca@gmail.com>
2014-11-17 23:58:09 -0500

    Allow hiding the post markup dropdown by setting a default renderer

--- dinette/forms.py
+++ dinette/forms.py
@@ -7,8 +7,17 @@ from dinette.models import Ftopics ,Reply
 
 #create a form from this Ftopics and use this when posting the a Topic
 class FtopicForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(FtopicForm, self).__init__(*args, **kwargs)
+        default_markup_type = getattr(settings, 'DEFAULT_MARKUP_TYPE', None)
+        if default_markup_type:
+            self.fields['message_markup_type'] = forms.CharField(
+                widget=forms.HiddenInput, initial=default_markup_type
+            )
+
     subject = forms.CharField(widget = forms.TextInput(attrs={"size":90}))
     message = forms.CharField(widget = forms.Textarea(attrs={"cols":90, "rows":10}))
+
     class Meta:
         model = Ftopics
         if getattr(settings, 'DINETTE_DISABLE_UPLOADS', False):
@@ -19,7 +28,16 @@ class FtopicForm(ModelForm):
 
 #create a form from Reply
 class ReplyForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ReplyForm, self).__init__(*args, **kwargs)
+        default_markup_type = getattr(settings, 'DEFAULT_MARKUP_TYPE', None)
+        if default_markup_type:
+            self.fields['message_markup_type'] = forms.CharField(
+                widget=forms.HiddenInput, initial=default_markup_type
+            )
+
     message = forms.CharField(widget = forms.Textarea(attrs={"cols":90, "rows":10}))
+
     class Meta:
         model = Reply
         if getattr(settings, 'DINETTE_DISABLE_UPLOADS', False):
